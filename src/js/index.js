21991ec23f07447fe3595996ec695cb0cf0c6996
rosa maria <rosadevfrontend@gmail.com>
2024-04-09 16:52:59 -0300

    adicionado comentarios

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+/*
+Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+        Passo 1 - pegar o botão mostra mais no JS para poder verificar quando i usuário clicar em cima dele 
+
+        Passo 2 - identificar o clicar no botão 
+
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos 
+   
+Objetivo 2- esconder o botão de mostrar mais 
+
+   Passo 1 - pega o botão e esconder ele      
+*/
+
+//Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+   
+//Passo 1 - pegar o botão mostra mais no JS para poder verificar quando i usuário clicar em cima dele 
+
+console.log(document);
