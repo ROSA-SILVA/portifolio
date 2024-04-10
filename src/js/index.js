302fd94717ab9d7f75c130129fac05ea043659fd
rosa maria <rosadevfrontend@gmail.com>
2024-04-10 15:47:17 -0300

    alteracoes
    fazendo pequenas alteraçoes  no javascript , e adicionando comentarios

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,7 @@ Objetivo 2- esconder o botão de mostrar mais
 */
 
 //Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
-   
+
 //Passo 1 - pegar o botão mostra mais no JS para poder verificar quando i usuário clicar em cima dele 
 
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
@@ -24,8 +24,8 @@ botaoMostrarProjetos.addEventListener('clik', () => {
     projetosInativos.forEach(projetosInativo => {
         projetosInativo.classList.add('ativo');
     });
-   
+
     //Objetivo 2- esconder o botão de mostrar mais 
     //  Passo 1 - pega o botão e esconder ele  
     botaoMostrarProjetos.classList.add("remover");
-})
+});
